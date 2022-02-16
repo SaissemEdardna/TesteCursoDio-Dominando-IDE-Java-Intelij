c03a5e9028237b615f29995a82fa6736492c7127
SaissemEdardna <msantanaandrade@outlook.com.br>
2022-02-16 11:37:36 -0300

    First Commit

--- src/br/com/dio/primeiroprograma.java
+++ src/br/com/dio/primeiroprograma.java
@@ -8,9 +8,5 @@ public class primeiroprograma {
         Gato gato = new Gato();
 
         System.out.println(gato);
-
-        /*  int a = 5;
-        int b = 3;
-        System.out.println("Hello World!" + (a+b));*/
     }
 }
